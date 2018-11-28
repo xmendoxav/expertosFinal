c0fe383ef5929e62add35e77471a918f857db62c
xmendoxav <37055047+xmendoxav@users.noreply.github.com>
2018-11-28 16:21:23 -0600

    as
    as

--- autoBar/application/models/ModelsP.php
+++ autoBar/application/models/ModelsP.php
@@ -4,26 +4,28 @@
     $this->load->database();
   }
 
-  public function insertaUsr($name, $apellidos, $email, $age, $sexo, $psw,$nacimiento){ //Inserts one User into de DataBase
-
-		//one argument is null "place of birth"
-  	$query = "INSERT INTO usuario (id, psw, nombre, apellidos,luga_nacimiento,edad,sexo,email) VALUES (NULL, '".$psw."', '".$name."', '".$apellidos."', '".$nacimiento."', '".$age."', '".$sexo."', '".$email."')";
-		return $this->db->query($query);
+  public function insertaUsr($name, $apellidos, $email, $age, $sexo, $psw, $l_nac){ //Inserts one User into de DataBase
+  //one argument is null "place of birth"
+  	$query = "INSERT INTO usuario (id, psw, nombre, apellidos,luga_nacimiento,edad,sexo,email) VALUES (NULL, '".$psw."', '".$name."', '".$apellidos."', '".$l_nac."', '".$age."', '".$sexo."', '".$email."')";
+  	return $this->db->query($query);
   }
-	public function buscausr($nombre, $psw){
-		$query = "Select * from usuario where nombre = '".$nombre."' and psw = '".$psw."';";
-		$resultado = $this->db->query($query);
-		return $resultado->row_array();
-	}
-	public function obtenInfo($nombre){
-		$query = "Select * from consumo where id_usuario = '".$nombre."'";
-		$resultado = $this->db->query($query);
-		return $resultado->result_array();
-	}
-	public function buscaPlatillos($tipo){
-		$query = "SELECT * FROM menu WHERE tipo_comida = '".$tipo."'";
-		$resultado = $this->db->query($query);
-		return $resultado->result_array();
-	}
-	}
+
+  public function buscausr($nombre, $psw){
+  	$query = "Select * from usuario where nombre = '".$nombre."' and psw = '".$psw."';";
+  	$resultado = $this->db->query($query);
+  	return $resultado->row_array();
+  }
+
+  public function obtenInfo($nombre){
+  	$query = "Select * from consumo where id_usuario = '".$nombre."'";
+  	$resultado = $this->db->query($query);
+  	return $resultado->result_array();
+  }
+
+  public function buscaPlatillos($tipo){
+  	$query = "SELECT * FROM menu WHERE tipo_comida = '".$tipo."'";
+  	$resultado = $this->db->query($query);
+  	return $resultado->result_array();
+  }
+}
 ?>
