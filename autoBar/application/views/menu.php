b2230b60dac26d94195eba4266a7905c5fe0b699
xmendoxav <37055047+xmendoxav@users.noreply.github.com>
2018-11-28 14:10:15 -0600

    Update menu.php
    a

--- autoBar/application/views/menu.php
+++ autoBar/application/views/menu.php
@@ -107,11 +107,12 @@
         <?php for ($i=0; $i <count($cenas) ; $i++) { ?>
         <div class="dinner menu-restaurant">
             <span class="clearfix">
-              <a class="menu-title" href="#" data-meal-img="assets/img/restaurant/rib.jpg"><?php echo $cenas[$i]["nombre"] ?></a>
+              <a class="menu-title" href="#" data-toggle="modal" data-target="#myModal" data-meal-img="assets/img/restaurant/rib.jpg"><?php echo $cenas[$i]["nombre"] ?></a>
               <span style="left: 166px; right: 44px;" class="menu-line"></span>
               <span class="menu-price">$<?php echo $cenas[$i]["precio"] ?></span>
             </span>
             <span class="menu-subtitle"><?php echo $cenas[$i]["descripcion"] ?></span>
+
           </div>
         <?php } ?>
         </div>
@@ -130,6 +131,25 @@
       </div>
     </div>
   </section>
+  <div id="myModal" class="modal fade" role="dialog">
+<div class="modal-dialog">
+    <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>This is a small modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   <!--/ menu -->
 
   <!-- footer -->
